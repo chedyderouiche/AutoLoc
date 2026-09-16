d7755ce3e084ed9908df48e616093471a433e4a1
chedyderouiche <chedy.derouiche@esprit.tn>
2026-09-16 12:35:00 +0100

    feat: Spring Boot backend configured and running on port 8081

---
+++ Desktop/archi SI/autoloc/src/main/java/tn/esprit/autoloc/autoloc_backend/AutolocBackendApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.autoloc.autoloc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutolocBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutolocBackendApplication.class, args);
+	}
+
+}
---
+++ Desktop/archi SI/autoloc/src/test/java/tn/esprit/autoloc/autoloc_backend/AutolocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.autoloc.autoloc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutolocBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
